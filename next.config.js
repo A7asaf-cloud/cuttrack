cca2be243f49fbd7943424014e4dbea262ca8cda
Asaf izyaguev <asafiz2002@gmail.com>
2026-08-05 01:33:35 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['your-supabase-project-id.supabase.co'], // replace later
+  },
+}
+
+module.exports = nextConfig
